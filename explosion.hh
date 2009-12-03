b8dc99877a39c641452103a5e305554b9ad6ed1a
Vegard Nossum <vegard.nossum@gmail.com>
2009-12-03 11:44:08 +0100

    Slow down explosions and let them explode other nearby ships
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- explosion.hh
+++ explosion.hh
@@ -15,6 +15,7 @@ public:
 	void remove_from_space(cpSpace *s);
 
 public:
+	unsigned int _switch_frame;
 	unsigned int _frame;
 
 	cpBody *_body;
@@ -22,13 +23,16 @@ public:
 };
 
 explosion::explosion(cpVect p, cpVect v):
+	_switch_frame(0),
 	_frame(0)
 {
 	_body = cpBodyNew(1, 10);
 	_body->p = p;
 	_body->v = v;
-	_shape = cpCircleShapeNew(_body, 8, cpvzero);
+	_shape = cpCircleShapeNew(_body, 16, cpvzero);
 	_shape->data = this;
+	_shape->collision_type = COLLISION_TYPE_BULLET;
+	_shape->group = COLLISION_GROUP_BULLET;
 	_shape->e = 0;
 	_shape->u = 0.5;
 }
@@ -54,8 +58,12 @@ explosion::draw()
 {
 	draw_sprite(explosion_texture[_frame], _body->p.x, _body->p.y, 0);
 
-	if (++_frame == 6)
-		removed_objects.insert(this);
+	if (++_switch_frame == CONFIG_FPS / 10) {
+		_switch_frame = 0;
+
+		if (++_frame == 6)
+			removed_objects.insert(this);
+	}
 }
 
 void
